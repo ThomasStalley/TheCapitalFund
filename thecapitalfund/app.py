b772dfcf0d20ee28a0f2c5411379b755ccc943d7
Thomas Stalley <109746564+ThomasStalley@users.noreply.github.com>
2025-10-09 11:36:40 +0100

    Update app.py

--- thecapitalfund/app.py
+++ thecapitalfund/app.py
@@ -11,7 +11,7 @@ def main():
     app = Dash(__name__, external_stylesheets=[BOOTSTRAP])
     app.title = "Auchester Asset Management"
     app.layout = layout.get_layout()
-    app.run_server(host="0.0.0.0", port=int(os.environ["PORT"]), debug=False, use_reloader=False)
+    app.run_server(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)) , debug=False, use_reloader=False)
 
 
 if __name__ == "__main__":
